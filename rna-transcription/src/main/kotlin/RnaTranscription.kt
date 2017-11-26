83db8b4a152888df4906f9bcbbf69fb3bcc58d25
Devin Brown <devin.g.brown@gmail.com>
2017-11-26 14:23:52 -0700

    I must enjoy torturing myself

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,4 +1,13 @@
-fun transcribeToRna(dna: String): String = ""
-
-fun cytosineComplementIsGuanine(nuclotide: String): String = nuclotide.replace("", "")
+fun transcribeToRna(dna: String): String {
+  return dna.map {
+    when(it) {
+      'G' -> "C"
+      'C' -> "G"
+      'T' -> "A"
+      'A' -> "U"
+      'U' -> "A"
+      else -> throw IllegalArgumentException("Invalid nucleotide.")
+    }
+  }.joinToString("")
+}
 
